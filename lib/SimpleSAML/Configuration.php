6f667b5c6fc6ebc69cb6493b23a95b9d570001a4
Tim van Dijen <tvdijen@gmail.com>
2022-02-05 12:22:09 +0100

    Refactor getString/getOptionalString

--- lib/SimpleSAML/Configuration.php
+++ lib/SimpleSAML/Configuration.php
@@ -606,36 +606,47 @@ class Configuration implements Utils\ClearableState
     /**
      * This function retrieves a string configuration option.
      *
-     * An exception will be thrown if this option isn't a string, or if this option isn't found, and no default value
-     * is given.
+     * An exception will be thrown if this option isn't a string, or if this option isn't found.
      *
-     * @param string $name The name of the option.
-     * @param mixed  $default A default value which will be returned if the option isn't found. The option will be
-     *                  required if this parameter isn't given. The default value can be any value, including
-     *                  null.
+     * @param string $name  The name of the option.
+     * @return string       The option with the given name.
      *
-     * @return string|mixed The option with the given name, or $default if the option isn't found and $default is
-     *     specified.
-     *
-     * @throws \Exception If the option is not a string.
+     * @throws \SimpleSAML\Assert\AssertionFailedException If the option is not a string.
      */
-    public function getString(string $name, $default = self::REQUIRED_OPTION)
+    public function getString(string $name): string
     {
-        $ret = $this->getValue($name, $default);
+        $ret = $this->getValue($name);
 
-        if ($ret === $default) {
-            // the option wasn't found, or it matches the default value. In any case, return this value
-            return $ret;
-        }
-
-        if (!is_string($ret)) {
-            throw new \Exception(
-                $this->location . ': The option ' . var_export($name, true) .
-                ' is not a valid string value.'
-            );
-        }
+        Assert::string(
+            $ret,
+            sprintf('%s: The option %s is not a valid string value.', $this->location, var_export($name, true)),
+        );
 
         return $ret;
+    }
+
+
+    /**
+     * This function retrieves an optional string configuration option.
+     *
+     * An exception will be thrown if this option isn't a string.
+     *
+     * @param string       $name     The name of the option.
+     * @param string|null  $default  A default value which will be returned if the option isn't found.
+     *                               The default value can be null or a string.
+     *
+     * @return string|null The option with the given name, or $default if the option isn't found.
+     *
+     * @throws \SimpleSAML\Assert\AssertionFailedException If the option is not a string.
+     */
+    public function getOptionalString(string $name, ?string $default): ?string
+    {
+        if (!$this->hasValue($name)) {
+            // the option wasn't found, or it matches the default value. In any case, return this value
+            return $default;
+        }
+
+        return $this->getString($name);
     }
 
 
